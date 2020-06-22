13887750dca55ea5279b51677f0a911d6e525ef8
Piyush Agarwal <51760520+Piyush55dude@users.noreply.github.com>
2020-06-22 17:33:45 +0530

    Updates

---
+++ precache-manifest.7a4ee059681c66028f6153aa5756f45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c514ed966858f0ffff7d0f74f5dc2266",
+    "url": "/Budget-Calculator/index.html"
+  },
+  {
+    "revision": "319cd015296036e09fe2",
+    "url": "/Budget-Calculator/static/css/main.0e2bcaf0.chunk.css"
+  },
+  {
+    "revision": "c59d27f9d06f40ffbb64",
+    "url": "/Budget-Calculator/static/js/2.cc0f10b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-Calculator/static/js/2.cc0f10b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319cd015296036e09fe2",
+    "url": "/Budget-Calculator/static/js/main.2190ee97.chunk.js"
+  },
+  {
+    "revision": "fcd4c169a3d3dd9e7ebd",
+    "url": "/Budget-Calculator/static/js/runtime-main.0e20bced.js"
+  }
+]);
